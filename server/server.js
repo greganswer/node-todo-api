a06a80550628ebc1b443bf28ba97ff03d0aec245
Greg Answer <greganswer@gmail.com>
2017-07-06 01:33:46 -0400

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
 const { ObjectID } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 const app = express();
 app.use(bodyParser.json());
 
@@ -70,6 +71,33 @@ app.delete('/todos/:id', (req, res) => {
   }
 
   Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send('Todo not found');
+      }
+      res.send({ todo });
+    })
+    .catch(e => res.status(400).send('Something went wrong'));
+});
+
+// PATCH /todos/:id
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('Invalid ID');
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
     .then(todo => {
       if (!todo) {
         return res.status(404).send('Todo not found');
